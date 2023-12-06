adf08b14159643057f71a289e399cf5e0ca63ce9
Rafaelokra <122841125+rafaelokra@users.noreply.github.com>
2023-12-06 00:38:10 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import GlobalStyles from './styles/global'
 
 import theme from './styles/theme'
 
-import { Routes } from './routes'
+import { Routes} from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
