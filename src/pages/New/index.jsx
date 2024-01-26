29ebfde94522e8b1d57f18e727d7cefa0a209c90
Rafaelokra <122841125+rafaelokra@users.noreply.github.com>
2024-01-25 23:33:26 -0300

    Revover tag

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -32,6 +32,10 @@ export function New() {
     setNewTag("");
   }
 
+  function handleRemoveTag(deleted){
+    setTags(prevState => prevState.filter((tag) => tag !== deleted ));
+  }
+
   
 
   return (
@@ -78,7 +82,7 @@ export function New() {
                   <NoteItem
                     key={String(index)}
                      value={tag}
-                     onClick={() => { }}
+                     onClick={() => { handleRemoveTag(tag)}}
                     />
                 ))
               }
