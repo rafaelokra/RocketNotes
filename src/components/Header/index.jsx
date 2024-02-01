6aa7cf71fe9b5f7d5b02256da53266600e46e7b2
Rafaelokra <122841125+rafaelokra@users.noreply.github.com>
2024-02-01 00:21:05 -0300

    TRedirecionando o Logout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,19 @@
 import { RiShutDownLine } from 'react-icons/ri';
+
 import { Container, Profile, Logout  } from './styles';
 import { useAuth } from "../../hooks/auth";
 import { api } from "../../Services/api"
 import  AvatarPlaceholder from "../../assets/avatar_placeholder.svg";
+import { useNavigate } from 'react-router-dom';
 
 export function Header() {
   const { signOut, user } = useAuth();
+  const navigation = useNavigate();
+
+  function handleSignOut(){
+    navigation("/");
+    signOut();
+  }
 
   const avatarUrl = user.avatar  ? `${api.defaults.baseURL}/files/${user.avatar}` :  AvatarPlaceholder;
 
@@ -24,7 +32,7 @@ export function Header() {
         </div>
 
       </Profile>
-      <Logout onClick={signOut}>
+      <Logout onClick={handleSignOut}>
         <RiShutDownLine />
       </Logout>
 
