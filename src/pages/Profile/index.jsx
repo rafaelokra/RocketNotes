02017c91448119521467017080760c0dc24aa28b
Rafaelokra <122841125+rafaelokra@users.noreply.github.com>
2024-01-31 23:50:43 -0300

    Ajuste avatar

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -29,13 +29,16 @@ function handleBack(){
 }
 
 async function handleUpdate(){
-  const user = {
+  const updated = {
     name,
     email,
     password: passwordNew,
     old_password: passwordOld,
   }
-  await updateProfile({ user, avatarFile });
+    const userUpdated = Object.assign(user, updated);
+   
+
+    await updateProfile({ user: userUpdated, avatarFile });
 }
 
 function handleChangeAvatar(event) {
