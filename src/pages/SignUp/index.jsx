baa35d29a3a4ff1a577e2fbc7dc77340f0793793
Rafaelokra <122841125+rafaelokra@users.noreply.github.com>
2024-02-05 22:48:35 -0300

    corrigindo

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from 'react-router-dom'
 import { api } from '../../Services/api';
 import { useState } from 'react'
 import { Input } from '../../components/Input'
-import { Button } from '../../components/Button'
+import { Button } from '../../components/button'
 
 import { Container, Form, Background } from './style'
 
